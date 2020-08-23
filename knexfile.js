e0b57ef88fbdc5620edd0dc1873f115fdb5390ba
ryo <ryoutakaya3623@Yahoo.co.jp>
2020-08-23 12:38:25 +0900

    change knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -11,33 +11,27 @@ module.exports = {
   },
   staging: {
     client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./db/migrations",
+      tableName: "knex_migrations",
     },
     pool: {
       min: 2,
       max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
     },
   },
 
   production: {
     client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./db/migrations",
+      tableName: "knex_migrations",
     },
     pool: {
       min: 2,
       max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
     },
   },
 };
